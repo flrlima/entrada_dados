4b118e1cd50f3f5bbb07530a6ab6e09c53af7bf4
Fabio Lima <flrlima1991@gmail.com>
2023-10-04 02:53:07 -0300

    entarda de dados

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// import 'package:entrada_de_dados/CampoTexto.dart';
+// import 'package:entrada_de_dados/EntradaCheckbox.dart';
+// import 'package:entrada_de_dados/EntradaRadioButton.dart';
+import 'package:entrada_de_dados/EntradaSlider.dart';
+// import 'package:entrada_de_dados/EntradaSwitch.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    //home: CampoTexto(),
+    //home: EntradaCheckbox(),
+    //home: EntradaRadioButton(),
+    //home: EntradaSwitch(),
+    home: EntradaSlider(),
+  ));
+}
